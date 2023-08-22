df7a28ae556f04e392ddf878589d497fdaf7d733
Benoit Dassy <benoit.dassy@eezee-it.com>
2023-08-22 14:25:41 +0200

    Form to create a new dragon - no layout

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -1,2 +1,22 @@
 class DragonsController < ApplicationController
+
+  def new
+    @dragon = Dragon.new
+  end
+
+  def create
+    @dragon = Dragon.new(dragon_params)
+    @dragon.user = current_user
+    if @dragon.save
+      redirect_to dragon_path(@dragon)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dragon_params
+    params.require(:dragon).permit(:name, :location, :category, :price_per_day, :description, :seats, :age)
+  end
 end
--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -1,4 +1,9 @@
 class Dragon < ApplicationRecord
+  CATEGORY = %w[Wyvern Wyrm Quetzalcoatl Hydra Drake]
+
   has_many :bookings
   belongs_to :user
+
+  validates :name, :category, :location, :price_per_day, :description, :seats, :age, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end

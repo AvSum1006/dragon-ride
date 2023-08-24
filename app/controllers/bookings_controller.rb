d9be7772ad8f25ba417c6ea22f260451be3295af
Benoit Dassy <benoit.dassy@eezee-it.com>
2023-08-24 12:11:33 +0200

    Booking new + some upgrade on view dragon

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,12 +8,37 @@ class BookingsController < ApplicationController
   def show
   end
 
+  def new
+    @dragon = Dragon.find(params[:dragon_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @dragon = Dragon.find(params[:dragon_id])
+    @booking = Booking.new(booking_params)
+    @booking.dragon = @dragon
+    @booking.user = current_user
+    @booking.status = "Pending"
+    duration = ((DateTime.parse(booking_params[:end_date]).to_date - DateTime.parse(booking_params[:start_date]).to_date).to_i) + 1
+    @booking.duration = duration
+    if @booking.save
+      flash[:notice] = "Booking successfully created"
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @booking.destroy
     redirect_to bookings_path, status: :see_other
   end
 
   private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 
   def set_booking
     @booking = Booking.find(params[:id])
